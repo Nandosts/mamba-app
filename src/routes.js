c0bf13879b92a02c9bce97b8a87134df5c318c7e
Fernando Jorge <fernando.jsts@gmail.com>
2022-09-04 18:34:10 -0300

    :sparkles: add personDetails page

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
 import Home from './routes/Home.html';
 import Blank from './routes/Blank.html';
+import PersonDetails from './routes/PersonDetails.html';
 
 const routes = {
   '/': Home,
   '/blank': Blank,
+  '/PersonDetails': PersonDetails,
 };
 
 export default routes;
